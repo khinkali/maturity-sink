29b5791ee9b8a543a01ddd0c71461143e9b5813e
Robert Brem <brem_robert@hotmail.com>
2018-11-15 20:46:08 +0100

    [FEAT] add date and array functionality

--- src/main/java/ch/adesso/maturity/board/metadata/entity/Metadata.java
+++ src/main/java/ch/adesso/maturity/board/metadata/entity/Metadata.java
@@ -11,7 +11,6 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
